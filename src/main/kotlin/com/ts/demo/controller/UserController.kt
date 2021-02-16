871087e9c805ccabfdb79e0c0d7aacf2cccdc516
ld3l <ld3l@yandex.ru>
2021-02-16 10:15:37 +0300

    rename method

--- src/main/kotlin/com/ts/demo/controller/UserController.kt
+++ src/main/kotlin/com/ts/demo/controller/UserController.kt
@@ -2,18 +2,25 @@ package com.ts.demo.controller
 
 import com.ts.demo.domain.Order
 import com.ts.demo.service.OrderService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @RestController()
 @RequestMapping("/user")
 class UserController(
     private val orderService: OrderService
 ) {
+//    @GetMapping("/{id}/orders")
+//    fun getOrderByUser(@PathVariable id: Int): Flux<Order> {
+//       return orderService.getAllOrdersByUserId(id)
+//    }
+
     @GetMapping("/{id}/orders")
-    fun getOrderByUser(@PathVariable id: Int): Flux<Order> {
-       return orderService.getAllOrdersByUserId(id)
+    fun getAllUserOrders(@PathVariable id: Int): Flux<Order> {
+        return orderService.getAllOrdersByUserId(id);
     }
 
     @PostMapping("/{id}/orders")
